eb037f1547923d191a1c0f843e1ef033485a60f2
admin <avenew@naver.com>
2020-08-11 17:48:34 +0900

    8_11 commit

---
+++ firstProject/src/com/yedam/test/test5.java
@@ -0,0 +1,17 @@
+package com.yedam.test;
+
+public class test5 {
+	public static void main(String[] args) {
+		byte byteValue = 10;
+		char charValue = 'A';
+	
+	int intValue = byteValue;
+	int intValue2 = charValue;
+	short shortVaue = charValue;
+	double doubleValue = byteValue;
+
+	}
+	
+
+
+}
