b734afe0539031f5515bbea0f4116a4f60327232
Administrator <Administrator@39.121.211.172>
2020-08-11 22:20:08 +0900

    homework

---
+++ firstProject/src/com/home/DenyLogicOperatorExample.java
@@ -0,0 +1,14 @@
+package com.home;
+
+public class DenyLogicOperatorExample {
+	public static void main(String[] args) {
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+	}
+}
