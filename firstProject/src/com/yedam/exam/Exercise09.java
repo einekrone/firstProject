2f03413adc30b2696900f5e34238d6432530003a
Administrator <Administrator@39.116.246.208>
2020-08-13 23:57:30 +0900

    8_13

--- firstProject/src/com/yedam/exam/Exercise09.java
+++ firstProject/src/com/yedam/exam/Exercise09.java
@@ -4,15 +4,13 @@ public class Exercise09 {
 	public static void main(String[] args) {
 		int max = 0;
 		int [] ary = { 1,5,3,8,2};
-		for (int a = 0; a<ary.length; a++);
-			if(int[0] ary < int[1] ary)
 		
-		
-		
-		
-			 
-		
-		
-		
+		for (int a = 0; a<ary.length; a++) {
+			if(max<ary[a]) {
+				max = ary[a];
+			}
+		}
+		System.out.println("max:" + max);
+	
 	}
 }
--- firstProject/src/com/yedam/exam/Exercise10.java
+++ firstProject/src/com/yedam/exam/Exercise10.java
@@ -2,14 +2,23 @@ package com.yedam.exam;
 
 public class Exercise10 {
 	public static void main(String[] args) {
-		int[][] abc = { { 95, 86 }, { 83, 92, 96 }, { 78, 83, 93, 87, 88 } };
+		int[][] abc = 
+			{ { 95, 86 }, 
+			{ 83, 92, 96 }, 
+			{ 78, 83, 93, 87, 88 } 
+			};
+		
 		int sum = 0;
 		double avg = 0.0;
-					
-		for (int a : abc) {
-			sum += a;
+				
+		int a = 0;
+		for (int i = 0; i<abc.length; i++){
+			for(int j = 0; j<abc[i].length; j++){
+				sum += abc[i][j];
+			a++;
 		}
-		avg = (double) sum / abc.length;
+	}
+		avg = (double) sum / a;
 		System.out.println("sum:" + sum); 
 		System.out.println("avg:" + avg);
 	}
